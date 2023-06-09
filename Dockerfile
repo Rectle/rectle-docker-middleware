f8e518d4eef7caffb7d65d92fe666b1b4c8e233e
Michał Bilski <michubilu@wp.pl>
2023-06-09 20:19:01 +0200

    Structure rebuild & docker setup

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM node:20-alpine3.17
 
+WORKDIR /app
 COPY . .
-RUN npm install -g localtunnel
-RUN lt --port 3000 &
 RUN npm install
-CMD ["node", "index.js"]
+CMD ["npm", "run", "start"]
---
+++ index.js
@@ -0,0 +1,46 @@
+import { Server } from "socket.io";
+import localtunnel from "localtunnel";
+import { v4 as uuidv4 } from 'uuid';
+import http from "http"
+import sha256 from 'crypto-js/sha256.js';
+
+import publicNamespace from "./namespaces/public.js"
+import privateNamespace from "./namespaces/private.js"
+
+const createSocketServer = async () => {
+    const io = new Server(3000, { 
+        cors: {
+            origin: ["app.rectle.com", "localhost", "127.0.0.1", "::1"],
+            allowedHeaders: ["x-build", "x-token"],
+            credentials: true
+        },
+        
+     });
+    
+    publicNamespace.run(io)
+    privateNamespace.run(io)
+}
+
+
+const tunnel = await localtunnel({ 
+    port: 3000,
+    subdomain: `rectle-${uuidv4()}`
+});
+
+const URL = tunnel.url.replace("https://", `wss://`)
+
+console.log(URL)
+tunnel.on('close', () => {
+    // tunnels are closed
+});
+
+
+const SECRET = sha256(uuidv4()).toString()
+
+http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'application/json'})
+  res.write(JSON.stringify({
+    secret: SECRET
+  }));
+  res.end();
+}).listen(8080, createSocketServer)
